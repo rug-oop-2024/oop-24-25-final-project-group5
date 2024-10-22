54fcd9c29c0ce1e9994c739cce2fe386b46f1741
ffbilly <b.w.o.van.bilsen@student.rug.nl>
2024-10-22 14:44:08 +0200

    Implemented a constructor

--- autoop/core/ml/feature.py
+++ autoop/core/ml/feature.py
@@ -6,7 +6,9 @@ import numpy as np
 from autoop.core.ml.dataset import Dataset
 
 class Feature(BaseModel):
-    # attributes here
+    def __init__(self, name: str, type: Literal["categorical", "numerical"]):
+        self.name = name
+        self.type = type
 
     def __str__(self):
         raise NotImplementedError("To be implemented.")
