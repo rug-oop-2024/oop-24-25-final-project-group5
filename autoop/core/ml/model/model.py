c0ccf26ebc71743dd79a6392cdbbc7077e9775c8
ffbilly <b.w.o.van.bilsen@student.rug.nl>
2024-10-25 14:54:17 +0200

    Added base class for model

--- autoop/core/ml/model/model.py
+++ autoop/core/ml/model/model.py
@@ -1,10 +1,35 @@
-
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from autoop.core.ml.artifact import Artifact
 import numpy as np
 from copy import deepcopy
 from typing import Literal
 
-class Model:
-    pass # your code (attribute and methods) here
+class Model(ABC):
+    _parameters: dict = {}
+    _hyperparameters: dict = {}
+
+    @abstractmethod
+    def fit(self, observations: np.ndarray, ground_truth: np.ndarray) -> None:
+        pass
+
+    @abstractmethod
+    def predict(self, observations: np.ndarray) -> np.ndarray:
+        pass
+
+    @property
+    def parameters(self) -> dict:
+        return deepcopy(self._parameters)
     
+    @parameters.setter
+    def parameters(self, new_params: dict) -> None:
+        self._parameters.update(new_params)
+
+    @property
+    def hyperparameters(self) -> dict:
+        return deepcopy(self._hyperparameters)
+    
+    @hyperparameters.setter
+    def hyperparameters(self, new_hyperparams: dict) -> None:
+        self._hyperparameters.update(new_hyperparams)
+
+
