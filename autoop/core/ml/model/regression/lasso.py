f23f1bebe3cb821ed7d87064f256e9fd30bdc34f
ffbilly <b.w.o.van.bilsen@student.rug.nl>
2024-10-26 12:37:21 +0200

    Split parameters and hyperparameters in Lasso

--- autoop/core/ml/model/regression/lasso.py
+++ autoop/core/ml/model/regression/lasso.py
@@ -5,27 +5,31 @@ from sklearn.linear_model import Lasso
 
 # Inherit from Model
 class LassoWrapper(Model):
-    # Add Lasso model and required parameters to inherited __init__
-    def __init__(self):
+    def __init__(self, alpha=1.0, max_iter=1000, tol=0.0001, selection='cyclic'):
         super().__init__()
-        self._lasso_model = Lasso()
-        self._parameters = {
-            "alpha": self._lasso_model.alpha,
-            "max_iter": self._lasso_model.max_iter,
-            "tol": self._lasso_model.tol,
-            "selection": self._lasso_model.selection
+        #set hyperparameters
+        self.hyperparameters = {
+            "alpha": alpha,
+            "max_iter": max_iter,
+            "tol": tol,
+            "selection": selection
         }
+        #initialize Lasso model with hyperparameters
+        self._lasso_model = Lasso(
+            alpha = self.hyperparameters["alpha"],
+            max_iter = self.hyperparameters["max_iter"],
+            tol = self.hyperparmeters["tol"],
+            selection = self.hyperparameters["selection"]
+        )
 
     # Fit data, store coefficient and intercept in parameters
     def fit(self, observations: np.ndarray, ground_truth: np.ndarray) -> None:
         self._lasso_model.fit(observations, ground_truth)
-        self._parameters["coef_"] = self._lasso_model.coef_
-        self._parameters["intercept_"] = self._lasso_model.intercept_
+        #Store learned parameters
+        self.parameters = {
+            "coeff_": self._lasso_model.coef_,
+            "intercept_": self._lasso_model.intercept_
+        }
 
     def predict(self, observations: np.ndarray) -> np.ndarray:
         return self._lasso_model.predict(observations)
-
-    # Allow user to read parameters without modifying them
-    @property
-    def parameters(self):
-        return self._parameters
