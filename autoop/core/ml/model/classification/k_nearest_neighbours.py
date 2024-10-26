58a17d08ced1d8728eb8915d3855f15c3b129e2f
ffbilly <b.w.o.van.bilsen@student.rug.nl>
2024-10-26 12:24:12 +0200

    Split parameters and hyperparameters in KNN

--- autoop/core/ml/model/classification/k_nearest_neighbours.py
+++ autoop/core/ml/model/classification/k_nearest_neighbours.py
@@ -7,12 +7,12 @@ from pydantic import field_validator
 
 # Inherit from Model
 class KNearestNeighbors(Model):
-    # Add attribute k (public), initialize observations and ground_truth as None
     def __init__(self, k=3):
         super().__init__()
-        self.k = k
-        self.observations = None
-        self.ground_truth = None
+        #set k as a hyperparameter
+        self.hyperparameters = {"k": k}
+        #set observations and ground_truth to None in parameters
+        self.parameters = {"observations": None, "ground_truth": None}
 
     # Check that k >= 0
     @field_validator("k")
@@ -22,9 +22,7 @@ class KNearestNeighbors(Model):
 
     # Store observations and ground_truth in parameters
     def fit(self, observations: np.ndarray, ground_truth: np.ndarray):
-        self.observations = observations
-        self.ground_truth = ground_truth
-        self._parameters = {
+        self.parameters = {
             "observations": observations,
             "ground_truth": ground_truth
         }
@@ -35,20 +33,16 @@ class KNearestNeighbors(Model):
 
     # Predict a single point
     def _predict_single(self, observation: np.ndarray):
-        
+        #Access k
+        k = self.hyperparameters["k"]
         # Calculate distance between observation and every other point
-        distances = np.linalg.norm(self._parameters["observations"]
+        distances = np.linalg.norm(self.parameters["observations"]
                                    - observation, axis=1)
         # Sort the array of the distances and take first k
-        k_indices = np.argsort(distances)[:self.k]
+        k_indices = np.argsort(distances)[:k]
         # Check the label aka ground truth of those points
-        k_nearest_labels = [self._parameters["ground_truth"][i]
+        k_nearest_labels = [self.parameters["ground_truth"][i]
                             for i in k_indices]
         # Take the most common label and return it to the caller
         most_common = Counter(k_nearest_labels).most_common()
         return most_common[0][0]
-
-    # Allow user to read parameters without modifying them
-    @property
-    def parameters(self):
-        return self._parameters
