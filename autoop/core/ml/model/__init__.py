ee8b69d940ef1f55b4d246f9e41010db5955ee9b
ffbilly <b.w.o.van.bilsen@student.rug.nl>
2024-10-29 14:23:52 +0100

    Registered models in the model init py file

--- autoop/core/ml/model/__init__.py
+++ autoop/core/ml/model/__init__.py
@@ -1,13 +1,34 @@
 
 from autoop.core.ml.model.model import Model
 from autoop.core.ml.model.regression import MultipleLinearRegression
+from autoop.core.ml.model.regression import LassoRegression
+from autoop.core.ml.model.regression import PolynomialRegression
+from autoop.core.ml.model.classification import DecisionTreeClassification
+from autoop.core.ml.model.classification import RandomForestClassification
+from autoop.core.ml.model.classification import KNearestNeighborsClassification
 
-REGRESSION_MODELS = [
-] # add your models as str here
+from typing import Literal
 
-CLASSIFICATION_MODELS = [
-] # add your models as str here
+REGRESSION_MODELS = {
+    "Lasso": LassoRegression,
+    "MultipleLinear": MultipleLinearRegression,
+    "Polynomial": PolynomialRegression,
+}
 
-def get_model(model_name: str) -> Model:
-    """Factory function to get a model by name."""
-    raise NotImplementedError("To be implemented.")
+CLASSIFICATION_MODELS = {
+    "DecisionTree": DecisionTreeClassification,
+    "RandomForest": RandomForestClassification,
+    "KNearestNeighbors": KNearestNeighborsClassification,
+}
+
+def get_models() -> dict[str, Literal["regression", "classification"]]:
+    return {**{model: "regression" for model in REGRESSION_MODELS}, **{model: "classification" for model in CLASSIFICATION_MODELS}}
+
+def get_model(model_name: str) -> type[Model]:
+    if model_name in REGRESSION_MODELS:
+        return REGRESSION_MODELS[model_name]
+    elif model_name in CLASSIFICATION_MODELS:
+        return CLASSIFICATION_MODELS[model_name]
+    else:
+        raise ValueError(f"Model {model_name} not found")
+
