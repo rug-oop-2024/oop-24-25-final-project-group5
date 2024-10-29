146963bcf05d3d7cdb9c285f5aff5fe5264a2786
ffbilly <b.w.o.van.bilsen@student.rug.nl>
2024-10-29 14:23:37 +0100

    Fixed exports in model files

---
+++ autoop/core/ml/model/classification/__init__.py
@@ -0,0 +1,4 @@
+
+from autoop.core.ml.model.classification.decision_tree_classification import DecisionTreeClassification
+from autoop.core.ml.model.classification.k_nearest_neighbors_classification import KNearestNeighborsClassification
+from autoop.core.ml.model.classification.random_forest_classification import RandomForestClassification
--- autoop/core/ml/model/regression/__init__.py
+++ autoop/core/ml/model/regression/__init__.py
@@ -1,2 +1,4 @@
 
 from autoop.core.ml.model.regression.multiple_linear_regression import MultipleLinearRegression
+from autoop.core.ml.model.regression.lasso_regression import LassoRegression
+from autoop.core.ml.model.regression.polynomial_regression import PolynomialRegression
