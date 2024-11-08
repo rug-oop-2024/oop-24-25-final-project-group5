da5e5e3c4cd4c386dd2732689970c7955fe2613a
ffbilly <b.w.o.van.bilsen@student.rug.nl>
2024-11-08 12:49:43 +0100

    Docs for dataset

--- autoop/core/ml/dataset.py
+++ autoop/core/ml/dataset.py
@@ -1,14 +1,39 @@
-from autoop.core.ml.artifact import Artifact
-import pandas as pd
 import io
+
+import pandas as pd
+
+from autoop.core.ml.artifact import Artifact
 
 
 class Dataset(Artifact):
-    def __init__(self, *args, **kwargs):
+    """
+    Dataset class to store the data frame.
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        """
+        Initialize the dataset object.
+        """
         super().__init__(type="dataset", *args, **kwargs)
 
     @staticmethod
-    def from_dataframe(data: pd.DataFrame, name: str, asset_path: str, version: str="1.0.0"):
+    def from_dataframe(
+            data: pd.DataFrame,
+            name: str,
+            asset_path: str,
+            version: str = "1.0.0"
+    ) -> 'Dataset':
+        """
+        Create a dataset from a data frame.
+        Args:
+            data: data frame to be saved.
+            name: name of the dataset.
+            asset_path: path to save the dataset.
+            version: version of the dataset.
+
+        Returns:
+            The dataset object.
+        """
         return Dataset(
             name=name,
             asset_path=asset_path,
@@ -16,11 +41,22 @@ class Dataset(Artifact):
             version=version,
         )
 
-    def readAsDataFrame(self) -> pd.DataFrame:
+    def read_as_data_frame(self) -> pd.DataFrame:
+        """
+        Read the data frame from the dataset.
+        Returns: The data frame.
+        """
         bytes = super().read()
         csv = bytes.decode()
         return pd.read_csv(io.StringIO(csv))
 
     def save(self, data: pd.DataFrame) -> bytes:
+        """
+        Save the data frame as a csv file.
+        Args:
+            data: - data frame to be saved.
+
+        Returns: The bytes of the saved data frame.
+        """
         bytes = data.to_csv(index=False).encode()
         return super().save(bytes)
