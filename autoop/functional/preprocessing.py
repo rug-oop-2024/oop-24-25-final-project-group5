d3c9caedd33aa857e129f1d6f089ace9e87baed7
ffbilly <b.w.o.van.bilsen@student.rug.nl>
2024-11-08 12:50:18 +0100

    Update preprocessing.py

--- autoop/functional/preprocessing.py
+++ autoop/functional/preprocessing.py
@@ -1,12 +1,14 @@
 from typing import List, Tuple
 from autoop.core.ml.feature import Feature
 from autoop.core.ml.dataset import Dataset
-import pandas as pd
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 
-def preprocess_features(features: List[Feature], dataset: Dataset) -> list[Tuple[str, np.ndarray, dict]]:
+def preprocess_features(
+        features: List[Feature],
+        dataset: Dataset
+) -> list[Tuple[str, np.ndarray, dict]]:
     """Preprocess features.
     Args:
         features (List[Feature]): List of features.
@@ -16,17 +18,27 @@ def preprocess_features(features: List[Feature], dataset: Dataset) -> list[Tuple
         Each ndarray of shape (N, ...)
     """
     results = []
-    raw = dataset.readAsDataFrame()
+    raw = dataset.read_as_data_frame()
     for feature in features:
         if feature.type == "categorical":
             encoder = OneHotEncoder()
-            data = encoder.fit_transform(raw[feature.name].values.reshape(-1, 1)).toarray()
-            aritfact = {"type": "OneHotEncoder", "encoder": encoder.get_params()}
+            data = encoder.fit_transform(
+                raw[feature.name].values.reshape(-1, 1)
+            ).toarray()
+            aritfact = {
+                "type": "OneHotEncoder",
+                "encoder": encoder.get_params()
+            }
             results.append((feature.name, data, aritfact))
         if feature.type == "numerical":
             scaler = StandardScaler()
-            data = scaler.fit_transform(raw[feature.name].values.reshape(-1, 1))
-            artifact = {"type": "StandardScaler", "scaler": scaler.get_params()}
+            data = scaler.fit_transform(
+                raw[feature.name].values.reshape(-1, 1)
+            )
+            artifact = {
+                "type": "StandardScaler",
+                "scaler": scaler.get_params()
+            }
             results.append((feature.name, data, artifact))
     # Sort for consistency
     results = list(sorted(results, key=lambda x: x[0]))
